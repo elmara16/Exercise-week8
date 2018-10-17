66adf79d1543652c15a1e4aa51ac9028eaf39227
elmara16 <elmara16@ru.is>
2018-10-17 15:28:27 +0000

    ymislegt

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 // app.js
 
-const greeting = require('./src/greeting');
+const app = require("./src/api");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+ console.log("Server running on port " + PORT);
+});
 
-console.log(greeting('Totoro'));
---
+++ src/api.js
@@ -0,0 +1,16 @@
+// src/api.js
+const express = require("express");
+const app = express();
+const greeting = require("./greeting");
+app.get("/greeting/:name", (req, res) => {
+ // Fill in your code that:
+ // 1. set's the status code to 200
+ res.status(200);
+ // 2. and returns an object with the greeting
+ // gretting is key and req.params
+ res.send({greeting:greeting(req.params.name)}); 
+ 
+ // Hint: `req.params.name`
+});
+module.exports = app;
+
